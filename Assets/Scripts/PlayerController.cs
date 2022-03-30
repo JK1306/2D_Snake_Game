af8f7fd47e5c6d87fcd667db803f30c7a7e69e73
DESKTOP-5L5JHTL\Hp <jaikishore1997@gmail.com>
2022-03-30 10:16:34 +0530

    backup push

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,70 +8,79 @@ public class PlayerController : MonoBehaviour
     public Transform snakeBodyPrefab;
     public PlayerDirection playerDirection;
     List<Transform> snakeBodies;
-    Vector3 currentPosition;
+    Vector3 currentPosition,
+            boddyAppliedPosition;
     /*
         Working code with Fixed Timestamp 0.2
         Max. Allowed Timestamp 0.33
     */
     void Start()
     {
+        Application.targetFrameRate = 40;
         playerDirection = PlayerDirection.Right;
         snakeBodies = new List<Transform>();
         snakeBodies.Add(gameObject.transform);
     }
 
     private void Update() {
-        // playerMovement();
-        // bodyMovement();
-        for(int i = snakeBodies.Count-1; i>0; i--){
-            snakeBodies[i].position = snakeBodies[i-1].position;
+        if(Input.GetKey(KeyCode.D) && playerDirection != PlayerDirection.Left){
+            playerDirection = PlayerDirection.Right;
+        }else if(Input.GetKey(KeyCode.S) && playerDirection != PlayerDirection.Up){
+            playerDirection = PlayerDirection.Down;
+        }else if(Input.GetKey(KeyCode.A) && playerDirection != PlayerDirection.Right){
+            playerDirection = PlayerDirection.Left;
+        }else if(Input.GetKey(KeyCode.W) && playerDirection != PlayerDirection.Down){
+            playerDirection = PlayerDirection.Up;
         }
-        currentPosition = transform.position;
-        Debug.Log("Current Position : "+currentPosition);
-        if(playerDirection == PlayerDirection.Right){
-
-            currentPosition.x += movementSpeed + Time.deltaTime;
-
-        }else if(playerDirection == PlayerDirection.Left){
-
-            currentPosition.x -= movementSpeed + Time.deltaTime;
-
-        }else if(playerDirection == PlayerDirection.Up){
-
-            currentPosition.y += movementSpeed + Time.deltaTime;
-
-        }else if(playerDirection == PlayerDirection.Up){
-
-            currentPosition.y -= movementSpeed + Time.deltaTime;
-        }
-        transform.position = currentPosition;
-        Debug.Log("Updated Position : "+transform.position);
     }
 
-    void bodyMovement(){
-        Debug.Log("Vector2 : "+Vector2.right);
+    private void FixedUpdate() {
+        playerMovement();
+        for(int i = snakeBodies.Count-1; i>0; i--){
+            boddyAppliedPosition = snakeBodies[i-1].position;
+
+            if(playerDirection == PlayerDirection.Right){
+                boddyAppliedPosition.x -= (float)0.5;
+            }else if(playerDirection == PlayerDirection.Left){
+                boddyAppliedPosition.x += (float)0.5;
+            }else if(playerDirection == PlayerDirection.Up){
+                boddyAppliedPosition.y -= (float)0.5;
+            }else if(playerDirection == PlayerDirection.Down){
+                boddyAppliedPosition.y += (float)0.5;
+            }
+
+            // boddyAppliedPosition.x -= (float)0.5;
+            snakeBodies[i].position = boddyAppliedPosition;
+            // Debug.Log("Body "+i+" Position : "+snakeBodies[i].transform.position,snakeBodies[i]);
+        }
+
+        transform.position = currentPosition;
+        // Debug.Log("Head Position : "+snakeBodies[0].transform.position,snakeBodies[0]);
     }
 
     void playerMovement(){
-
+        currentPosition = transform.position;
+        if(playerDirection == PlayerDirection.Right){
+            currentPosition.x += movementSpeed * Time.deltaTime;
+        }else if(playerDirection == PlayerDirection.Left){
+            currentPosition.x -= movementSpeed * Time.deltaTime;
+        }else if(playerDirection == PlayerDirection.Up){
+            currentPosition.y += movementSpeed * Time.deltaTime;
+        }else if(playerDirection == PlayerDirection.Down){
+            currentPosition.y -= movementSpeed * Time.deltaTime;
+        }
     }
+
 
     void Grow(){
         Transform bodyObject = Instantiate(snakeBodyPrefab);
-        bodyObject.position = snakeBodies[snakeBodies.Count-1].position;
+        Vector2 appliedPosition = snakeBodies[snakeBodies.Count-1].position;
+        appliedPosition.x -= transform.localScale.x;
+        bodyObject.position = appliedPosition;
         snakeBodies.Add(bodyObject);
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        Debug.Log("Collided");
-        if(other.gameObject.name == "Food"){
-            Destroy(other.gameObject);
-            Grow();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("Trigger Collided");
         if(other.gameObject.name == "Food"){
             Destroy(other.gameObject);
             Grow();
---
+++ Assets/Scripts/ScreenManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenManager : MonoBehaviour
+{
+    static ScreenManager screenManagerInstance;
+    Vector2 minCoordinate,
+            maxCoordinate;
+    float mapX = 100,
+            mapY = 100;
+    public static ScreenManager instance{
+        get{
+            return screenManagerInstance;
+        }
+    }
+    void Start()
+    {
+        float vertExtent = Camera.main.orthographicSize;    
+        float horzExtent = vertExtent * Screen.width / Screen.height;
+ 
+        minCoordinate = new Vector2(
+            (float)(horzExtent - mapX / 2.0),
+            (float)(vertExtent - mapY / 2.0)
+        );
+        maxCoordinate = new Vector2(
+            (float)(mapX / 2.0 - horzExtent),
+            (float)(mapY / 2.0 - vertExtent)
+        );
+
+        Debug.Log("Minimum Coordinates : "+minCoordinate);
+        Debug.Log("Maximum Coordinates : "+maxCoordinate);
+
+        if( screenManagerInstance == null){
+            screenManagerInstance = this;
+        }else{
+            Destroy(this);
+        }
+    }
+}
